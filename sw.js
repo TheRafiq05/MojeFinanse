170e20e44d061dbc881bee4734aa19ba6b15d444
TheRafiq05 <rafaltokarczyk05@gmail.com>
2026-07-23 10:44:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,41 @@
-    <!-- Rejestracja Service Workera (PWA / Offline) -->
-    <script>
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('./sw.js')
-                    .then(registration => {
-                        console.log('ServiceWorker zarejestrowany pomyślnie!');
-                    })
-                    .catch(err => {
-                        console.log('Błąd rejestracji ServiceWorkera: ', err);
-                    });
-            });
-        }
-    </script>
+    const CACHE_NAME = 'mojefinanse-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalacja nowego Service Workera
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Zmusza telefon do natychmiastowego porzucenia starej wersji
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Aktywacja i czyszczenie STARYCH wersji (To naprawia Twój problem!)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Usuwam stary cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Pobieranie plików
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
